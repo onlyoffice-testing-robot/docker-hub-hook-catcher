a91225a91d0cc18b95d5296411a6892e06c370b1
Pavel Lobashov <shockwavenn@gmail.com>
2015-10-01 15:39:51 +0300

    Render json as html

--- hook-catcher/dockerhub_hook_catcher.rb
+++ hook-catcher/dockerhub_hook_catcher.rb
@@ -1,6 +1,10 @@
 require 'sinatra'
 require 'json'
 
+get '/' do
+  File.read(File.join('docker-hub.json', 'index.html'))
+end
+
 post '/payload' do
   push = JSON.parse(request.body.read)
   File.open('docker-hub.json', 'w') do |f|
