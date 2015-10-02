f650f31003b27cf4bb9a243fd9e991dc733be078
Pavel Lobashov <shockwavenn@gmail.com>
2015-10-02 15:20:50 +0300

    Fix path

--- hook-catcher/tmux-dockerhub-hook-catcher.sh
+++ hook-catcher/tmux-dockerhub-hook-catcher.sh
@@ -2,4 +2,4 @@
 tmux new-session -d -s dockerhub-hook-catcher
 tmux send-keys -t dockerhub-hook-catcher:0 "sleep 2m" C-m
 tmux send-keys -t dockerhub-hook-catcher:0 "cd ~/RubymineProjects/SharedFunctional/" C-m
-tmux send-keys -t dockerhub-hook-catcher:0 "ruby services/docker-hub/hook-catcher/tmux-dockerhub-hook-catcher.sh -o 0.0.0.0 -p 8080" C-m
+tmux send-keys -t dockerhub-hook-catcher:0 "ruby services/docker-hub/hook-catcher/dockerhub_hook_catcher.rb -o 0.0.0.0 -p 8080" C-m
