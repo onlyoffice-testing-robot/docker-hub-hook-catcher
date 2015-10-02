e5aae825b0b702a8e30514ad6e63986b896317b0
Pavel Lobashov <shockwavenn@gmail.com>
2015-10-02 15:21:03 +0300

    Add git pull

--- hook-catcher/tmux-dockerhub-hook-catcher.sh
+++ hook-catcher/tmux-dockerhub-hook-catcher.sh
@@ -2,4 +2,5 @@
 tmux new-session -d -s dockerhub-hook-catcher
 tmux send-keys -t dockerhub-hook-catcher:0 "sleep 2m" C-m
 tmux send-keys -t dockerhub-hook-catcher:0 "cd ~/RubymineProjects/SharedFunctional/" C-m
+tmux send-keys -t reinit-document-docker:0 "git pull" C-m
 tmux send-keys -t dockerhub-hook-catcher:0 "ruby services/docker-hub/hook-catcher/dockerhub_hook_catcher.rb -o 0.0.0.0 -p 8080" C-m
