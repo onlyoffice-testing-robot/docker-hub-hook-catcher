b3c1fe757113d25b60d7c18a1efd8a9f25d40c83
Pavel Lobashov <shockwavenn@gmail.com>
2015-10-02 15:26:53 +0300

    Fix paths for files

--- hook-catcher/tmux-dockerhub-hook-catcher.sh
+++ hook-catcher/tmux-dockerhub-hook-catcher.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 tmux new-session -d -s dockerhub-hook-catcher
 tmux send-keys -t dockerhub-hook-catcher:0 "sleep 2m" C-m
-tmux send-keys -t dockerhub-hook-catcher:0 "cd ~/RubymineProjects/SharedFunctional/" C-m
+tmux send-keys -t dockerhub-hook-catcher:0 "cd ~/RubymineProjects/SharedFunctional/services/docker-hub/hook-catcher/" C-m
 tmux send-keys -t dockerhub-hook-catcher:0 "git pull" C-m
-tmux send-keys -t dockerhub-hook-catcher:0 "ruby services/docker-hub/hook-catcher/dockerhub_hook_catcher.rb -o 0.0.0.0 -p 8080" C-m
+tmux send-keys -t dockerhub-hook-catcher:0 "ruby dockerhub_hook_catcher.rb -o 0.0.0.0 -p 8080" C-m
