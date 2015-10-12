7695c02e6a62ce9407fa4cf3d79ba4f88ad9f73d
Pavel Lobashov <shockwavenn@gmail.com>
2015-10-12 17:50:00 +0300

    Add rvm initialization to tmux start file

--- hook-catcher/tmux-dockerhub-hook-catcher.sh
+++ hook-catcher/tmux-dockerhub-hook-catcher.sh
@@ -5,4 +5,5 @@ tmux new-session -d -s dockerhub-hook-catcher
 tmux send-keys -t dockerhub-hook-catcher:0 "sleep 2m" C-m
 tmux send-keys -t dockerhub-hook-catcher:0 "cd ~/RubymineProjects/SharedFunctional/services/docker-hub/hook-catcher/" C-m
 tmux send-keys -t dockerhub-hook-catcher:0 "git pull" C-m
+tmux send-keys -t dockerhub-hook-catcher:0 "source ~/.rvm/scripts/rvm" C-m
 tmux send-keys -t dockerhub-hook-catcher:0 "ruby dockerhub_hook_catcher.rb -o 0.0.0.0 -p 8080" C-m
