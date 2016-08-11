844ba772698f0cc31b9a0a70e7fe99a801dcd076
Pavel Lobashov <shockwavenn@gmail.com>
2016-08-11 17:13:48 +0300

    Redirect get payload to root

--- dockerhub_hook_catcher.rb
+++ dockerhub_hook_catcher.rb
@@ -5,6 +5,10 @@ get '/' do
   send_file 'docker-hub.json'
 end
 
+get '/payload' do
+  redirect '/'
+end
+
 post '/payload' do
   push = JSON.parse(request.body.read)
   File.open('docker-hub.json', 'w') do |f|
